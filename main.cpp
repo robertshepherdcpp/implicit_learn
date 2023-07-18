4d07e8118b8bc37fa2f5d3750a4a926d215056ab
rshepherdcpp <113625774+robertshepherdcpp@users.noreply.github.com>
2023-07-18 09:21:05 +0100

    add some basic functionality

--- main.cpp
+++ main.cpp
@@ -7,12 +7,26 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(1920, 1001), "implicit learn");
 
     bool ShouldShowOpening = true;
+    bool ShouldShowOptions = false;
 
     sf::Texture opening_background_texture;
     opening_background_texture.loadFromFile("implicit_learn.png");
 
     sf::Sprite opening_background_sprite;
     opening_background_sprite.setTexture(opening_background_texture);
+
+    sf::Texture maths_texture;
+    maths_texture.loadFromFile("maths.png");
+    sf::Sprite maths(maths_texture);
+
+    sf::Texture physics_texture;
+    physics_texture.loadFromFile("physics.png");
+    sf::Sprite physics(physics_texture);
+
+    sf::Texture compsci_texture;
+    compsci_texture.loadFromFile("compsci.png");
+    sf::Sprite compsci(compsci_texture);
+
 
     while (window.isOpen())
     {
@@ -23,17 +37,49 @@ int main()
             {
                 window.close();
             }
-            if ((ShouldShowOpening) && (event.type == sf::Event::KeyReleased))
+            if (ShouldShowOpening)
             {
-                std::cout << "Window should close\n";
-                ShouldShowOpening = false;
+                if (event.type == sf::Event::KeyReleased)
+                {
+                    ShouldShowOpening = false;
+                    ShouldShowOptions = true;
+                }
+            }
+            else if (ShouldShowOptions)
+            {
+                maths.setPosition(  450,  250);
+                physics.setPosition(850,  250);
+                compsci.setPosition(1250, 250);
+
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    if (maths.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+                    {
+                        std::cout << "Maths clicked!" << std::endl;
+                    }
+                    if (physics.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+                    {
+                        std::cout << "Physics clicked!" << std::endl;
+                    }
+                    if (compsci.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+                    {
+                        std::cout << "Compsci clicked!" << std::endl;
+                    }
+                }
             }
         }
 
         window.clear();
         if (ShouldShowOpening)
         {
-            window.draw(opening_backgr
+            window.draw(opening_background_sprite);
+        }
+        if (ShouldShowOptions)
+        {
+            window.draw(maths);
+            window.draw(physics);
+            window.draw(compsci);
+        }
         window.display();
     }
 }
